9b0588b38b5a6e4a9c8b6634ea131e2fa20b87ba
Jet Yu <jetyu91@gmail.com>
2016-10-17 21:28:31 +0800

    update

--- FreeCodeCamp/Title Case a Sentence.js
+++ FreeCodeCamp/Title Case a Sentence.js
@@ -4,14 +4,11 @@
 2.将数组中的元素全部小写化。（toLowerCase()）
 3.使用replace(),toUpperCase()方法将每一项首字母大写化，之后join()就可以了。
  */
-
 function titleCase(str) {
-    var array=str.toLowerCase().split(" ");
-
+    var array = str.toLowerCase().split(" ");
     for (var i = 0; i < array.length; i++) {
-        array[i] = array[i].charAt(0).toUpperCase()
+        array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1).toLowerCase();
     }
-
+    return array.join(" ");
 }
-
 titleCase("I'm a little tea pot");
